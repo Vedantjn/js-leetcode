2ca902db2b35689ae4a1f6df146fe3baa16098ba
Vedantjn <vedant.jain.indore@gmail.com>
2024-09-24 09:58:26 +0530

    chunk array

---
+++ 21_chunk_array.ts
@@ -0,0 +1,14 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
+
+function chunk(arr: Obj[], size: number): Obj[][] {
+    let i: number = 0;
+    let result: Obj[][] = [];
+
+    while (i < arr.length) {
+        result.push(arr.slice(i, i + size));
+        i += size;
+    }
+
+    return result;
+}
